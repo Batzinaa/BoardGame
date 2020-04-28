5122e99ff948ab016d77d1c40cbc2b8a012643c6
Batzinaa <44440196+Batzinaa@users.noreply.github.com>
2020-04-28 18:09:33 +0300

    Delete Behaviour.java

--- src/main/java/org/example/behaviours/Behaviour.java
+++
@@ -1,7 +0,0 @@
-package org.example.behaviours;
-
-import org.example.model.BoardGame;
-
-public interface Behaviour {
-    void execute(BoardGame boardGame); //Decide later if, what parameters need to be passed and returned.
-}
