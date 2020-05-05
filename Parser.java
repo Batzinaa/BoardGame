e6eb9cfcdb2cc475b981899f21a9845665544b69
Antonisiak <64683902+Antonisiak@users.noreply.github.com>
2020-05-05 18:29:13 +0300

    Create Parser.java

---
+++ Parser.java
@@ -0,0 +1,58 @@
+# BoardGame
+Initial commit
+package test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ParseJSON 
+{
+
+	public static void main(String[] args) 
+	{
+		JSONParser parser = new JSONParser();
+		
+		
+		try
+		{
+			Object obj = parser.parse(new FileReader("test.json"));
+			JSONObject jsonObject = (JSONObject) obj;
+			
+			Object game = jsonObject.get("game configuration");
+			//System.out.println("Board size is :" + game);
+			
+			Object board = jsonObject.get("number of squares");
+			System.out.println("Board size : " + board);
+			
+			JSONArray squaresArray = (JSONArray) jsonObject.get("squares");
+			Iterator<Object> iterator2 = squaresArray.iterator();
+			
+			while(iterator2.hasNext())
+			{	
+				System.out.println("Square : " + iterator2.next());
+			}
+			
+			JSONArray playersArray = (JSONArray) jsonObject.get("players");
+			Iterator<String> iterator = playersArray.iterator();
+			
+			while(iterator.hasNext())
+			{	
+				System.out.println("Player : " + iterator.next());
+			}
+		}
+		
+		catch(FileNotFoundException e) {e.printStackTrace();}
+		catch(IOException e) {e.printStackTrace();}
+		catch(ParseException e) {e.printStackTrace();}
+		catch(Exception e) {e.printStackTrace();}
+
+	}
+
+}
